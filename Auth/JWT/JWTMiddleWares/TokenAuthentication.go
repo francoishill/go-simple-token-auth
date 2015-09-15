e766b557fb68ef45c27df5f872328084bbf58c2b
Francois <francoishill11@gmail.com>
2015-09-15 06:37:05 +0200

    Move the `RequireTokenAuthentication` unto a struct instead of using global variables.

--- Auth/JWT/JWTMiddleWares/TokenAuthentication.go
+++ Auth/JWT/JWTMiddleWares/TokenAuthentication.go
@@ -6,24 +6,32 @@ import (
 	"net/http"
 )
 
-var TokenHandlers iTokenHandlers = nil
-
 type iTokenHandlers interface {
 	HandleTokenExtractedSuccessfully(http.ResponseWriter, *http.Request, *jwt.Token)
 }
 
-func RequireTokenAuthentication(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+type JWTMiddleWares struct {
+	TokenHandlers iTokenHandlers
+}
+
+func (j *JWTMiddleWares) RequireTokenAuthentication(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
 	authBackend := JWTBackend.CurrentGlobalAuthenticationBackend
 
 	token, err := jwt.ParseFromRequest(req, authBackend.GetPublicKey)
 	//TODO: We should probably check for specific errors like 'jwt.ValidationErrorExpired'
 
 	if err == nil && token.Valid && !authBackend.IsInBlacklist(req.Header.Get("Authorization")) {
-		if TokenHandlers != nil {
-			TokenHandlers.HandleTokenExtractedSuccessfully(rw, req, token)
+		if j.TokenHandlers != nil {
+			j.TokenHandlers.HandleTokenExtractedSuccessfully(rw, req, token)
 		}
 		next(rw, req)
 	} else {
 		rw.WriteHeader(http.StatusUnauthorized)
+	}
+}
+
+func New(tokenHandlers iTokenHandlers) *JWTMiddleWares {
+	return &JWTMiddleWares{
+		tokenHandlers,
 	}
 }
